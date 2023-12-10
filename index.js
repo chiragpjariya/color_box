b9eeb9a8016c67dc9769d176eaf5545e321f736c
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2023-12-10 13:26:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,143 @@
+let Color = '0123456789ABCDEF';
+
+
+
+setInterval_fun = setInterval(() => {
+    let Hex = "#";
+    for (let index = 0; index < 6; index++) {
+        Hex += Color[Math.floor(Math.random() * 16)];
+    }
+    setTimeout(() => {
+        document.getElementById('box1').innerHTML = Hex;
+        document.getElementById('box1').style.backgroundColor = Hex;
+    }, 1000)
+    setTimeout(() => {
+        document.getElementById('box2').innerHTML = Hex;
+        document.getElementById('box2').style.backgroundColor = Hex;
+    }, 2000)
+    setTimeout(() => {
+        document.getElementById('box3').innerHTML = Hex;
+        document.getElementById('box3').style.backgroundColor = Hex;
+    }, 3000)
+    setTimeout(() => {
+        document.getElementById('box4').innerHTML = Hex;
+        document.getElementById('box4').style.backgroundColor = Hex;
+    }, 4000)
+    setTimeout(() => {
+        document.getElementById('box5').innerHTML = Hex;
+        document.getElementById('box5').style.backgroundColor = Hex;
+    }, 5000)
+    setTimeout(() => {
+        document.getElementById('box6').innerHTML = Hex;
+        document.getElementById('box6').style.backgroundColor = Hex;
+    }, 6000)
+    setTimeout(() => {
+        document.getElementById('box7').innerHTML = Hex;
+        document.getElementById('box7').style.backgroundColor = Hex;
+    }, 7000)
+    setTimeout(() => {
+        document.getElementById('box8').innerHTML = Hex;
+        document.getElementById('box8').style.backgroundColor = Hex;
+    },
+        8000)
+    setTimeout(() => {
+        document.getElementById('box9').innerHTML = Hex;
+        document.getElementById('box9').style.backgroundColor = Hex;
+    }, 9000)
+    setTimeout(() => {
+        document.getElementById('box10').innerHTML = Hex;
+        document.getElementById('box10').style.backgroundColor = Hex;
+
+    }, 10000)
+    /////////////////////////////////////////////////////////////////
+    setTimeout(() => {
+        document.getElementById('box11').innerHTML = Hex;
+        document.getElementById('box11').style.backgroundColor = Hex;
+    }, 11000)
+    setTimeout(() => {
+        document.getElementById('box12').innerHTML = Hex;
+        document.getElementById('box12').style.backgroundColor = Hex;
+    }, 12000)
+    setTimeout(() => {
+        document.getElementById('box13').innerHTML = Hex;
+        document.getElementById('box13').style.backgroundColor = Hex;
+    }, 13000)
+    setTimeout(() => {
+        document.getElementById('box14').innerHTML = Hex;
+        document.getElementById('box14').style.backgroundColor = Hex;
+    }, 14000)
+    setTimeout(() => {
+        document.getElementById('box15').innerHTML = Hex;
+        document.getElementById('box15').style.backgroundColor = Hex;
+    }, 15000)
+    setTimeout(() => {
+        document.getElementById('box16').innerHTML = Hex;
+        document.getElementById('box16').style.backgroundColor = Hex;
+    }, 16000)
+    setTimeout(() => {
+        document.getElementById('box17').innerHTML = Hex;
+        document.getElementById('box17').style.backgroundColor = Hex;
+    }, 17000)
+    setTimeout(() => {
+        document.getElementById('box18').innerHTML = Hex;
+        document.getElementById('box18').style.backgroundColor = Hex;
+    }, 18000)
+    setTimeout(() => {
+        document.getElementById('box19').innerHTML = Hex;
+        document.getElementById('box19').style.backgroundColor = Hex;
+    }, 19000)
+    setTimeout(() => {
+        document.getElementById('box20').innerHTML = Hex;
+        document.getElementById('box20').style.backgroundColor = Hex;
+    }, 20000)
+    /////////////////////////////////////////////////////////////////
+    setTimeout(() => {
+        document.getElementById('box21').innerHTML = Hex;
+        document.getElementById('box21').style.backgroundColor = Hex;
+    }, 21000)
+    setTimeout(() => {
+        document.getElementById('box22').innerHTML = Hex;
+        document.getElementById('box22').style.backgroundColor = Hex;
+    }, 22000)
+    setTimeout(() => {
+        document.getElementById('box23').innerHTML = Hex;
+        document.getElementById('box23').style.backgroundColor = Hex;
+    }, 23000)
+    setTimeout(() => {
+        document.getElementById('box24').innerHTML = Hex;
+        document.getElementById('box24').style.backgroundColor = Hex;
+    }, 24000)
+    setTimeout(() => {
+        document.getElementById('box25').innerHTML = Hex;
+        document.getElementById('box25').style.backgroundColor = Hex;
+    }, 25000)
+    setTimeout(() => {
+        document.getElementById('box26').innerHTML = Hex;
+        document.getElementById('box26').style.backgroundColor = Hex;
+    }, 26000)
+    setTimeout(() => {
+        document.getElementById('box27').innerHTML = Hex;
+        document.getElementById('box27').style.backgroundColor = Hex;
+    }, 27000)
+    setTimeout(() => {
+        document.getElementById('box28').innerHTML = Hex;
+        document.getElementById('box28').style.backgroundColor = Hex;
+    }, 28000)
+    setTimeout(() => {
+        document.getElementById('box29').innerHTML = Hex;
+        document.getElementById('box29').style.backgroundColor = Hex;
+    }, 29000)
+    setTimeout(() => {
+        document.getElementById('box30').innerHTML = Hex;
+        document.getElementById('box30').style.backgroundColor = Hex;
+    }, 30000)
+    //////////////////////////////////////////////////////////////
+
+}, 1000)
+
+
+
+
+
+
+
